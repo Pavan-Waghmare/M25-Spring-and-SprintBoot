cc6d3707af69e3ed6d4e57faaabeedac217893ca
admin <admin@DESKTOP-S6OFAAK>
2022-12-10 12:41:00 +0530

    First Commit

---
+++ SpringMVC/src/main/WebApp/Firsthome.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+pageEncoding="ISO-8859-1"%>
+<!DOCTYPE HTML>
+<HTML>
+<HEAD>
+<meta charset="ISO-8859-1">
+<title>Spring MVC Demo</title>
+</HEAD>
+<BODY>
+	<H1>MY NAME IS PAVAN</H1>
+</BODY>
+</HTML>	
--- SpringMVC/src/main/java/com/example/demo/SpringMvcApplication.java
+++ SpringMVC/src/main/java/com/example/demo/SpringMvcApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringStarterProjectApplication {
+public class SpringMvcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringStarterProjectApplication.class, args);
+		SpringApplication.run(SpringMvcApplication.class, args);
 	}
 
 }
